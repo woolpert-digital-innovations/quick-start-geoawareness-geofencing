39ea70bf7db0b40072e51f1ba9a5a9a0a1f94b62
David Hollema <david.hollema@woolpert.com>
2020-07-07 01:25:20 +0000

    Event ordering enforcement

--- test/test_utils.js
+++ test/test_utils.js
@@ -15,17 +15,17 @@ const createStore = storeName => {
 const createGeofences = () => {
     const geofences = [];
 
-    let rawPoly = fs.readFileSync('test/time_120_poly.json', 'utf8');
+    let rawPoly = fs.readFileSync('test/fakes/time_120_poly.json', 'utf8');
     let poly = JSON.parse(rawPoly);
     let geofence = createGeofence(120, poly);
     geofences.push(geofence);
 
-    rawPoly = fs.readFileSync('test/time_300_poly.json', 'utf8');
+    rawPoly = fs.readFileSync('test/fakes/time_300_poly.json', 'utf8');
     poly = JSON.parse(rawPoly);
     geofence = createGeofence(300, poly);
     geofences.push(geofence);
 
-    rawPoly = fs.readFileSync('test/time_600_poly.json', 'utf8');
+    rawPoly = fs.readFileSync('test/fakes/time_600_poly.json', 'utf8');
     poly = JSON.parse(rawPoly);
     geofence = createGeofence(600, poly);
     geofences.push(geofence);
