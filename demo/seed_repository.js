e6eadb62dfa7540e7155629453eba178fc3a8903
David Hollema <david.hollema@woolpert.com>
2020-07-09 10:08:40 -0600

    Add 2 additional stores to seeding script

--- demo/seed_repository.js
+++ demo/seed_repository.js
@@ -4,13 +4,35 @@ const utils = require('../test/test_utils');
 const storeName = 'Carmelit';
 const seedStore = async () => {
     await repository.deleteStore(storeName);
-    const store = {
+    let store = {
         name: storeName,
         location: {
             longitude: -93.220024,
             latitude: 36.650717
         },
         address: '504 Veterans Blvd, Branson, MO 65616'
+    }
+    await repository.insertStore(store);
+
+    await repository.deleteStore('Napoli');
+    store = {
+        name: 'Napoli',
+        location: {
+            latitude: 37.162649,
+            longitude: -93.30884
+        },
+        address: '3600 S Fort Ave, Springfield, MO 65807'
+    }
+    await repository.insertStore(store);
+
+    await repository.deleteStore('Montmartre');
+    store = {
+        name: 'Montmartre',
+        location: {
+            latitude: 36.126373,
+            longitude: -94.159907
+        },
+        address: '202 W Joyce Blvd, Fayetteville, AR 72703'
     }
     await repository.insertStore(store);
 }
@@ -43,7 +65,11 @@ const seedOrders = async () => {
     }
 }
 
-seedStore();
-seedGeofences();
-seedOrders();
-console.log(`Seeding completed for ${storeName}.`);
+const doSeeding = async () => {
+    await seedStore();
+    await seedGeofences();
+    await seedOrders();
+    console.log('Seeding script finished.');
+}
+
+doSeeding();
